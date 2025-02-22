b4b703755c78ff0bedc787a2a4df6662a0d3a74a
Sebastian Galindez <sebgal2008@gmail.com>
2025-02-22 23:48:46 +0100

    Make side bar collapsible

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import {
   IonIcon,
   IonLabel,
   IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonSplitPane,
+  IonButton,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { images, square, triangle } from 'ionicons/icons';
+import { images, square, triangle, chevronBack, chevronForward } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -34,43 +40,76 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Custom styles for the menu */
+import './theme/menu.css';
+import { useEffect, useState } from 'react';
+
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={images} />
-            <IonLabel>Photos</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [isMenuCollapsed, setIsMenuCollapsed] = useState(false);
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonSplitPane contentId="main">
+          <IonMenu contentId="main" disabled={isMenuCollapsed}>
+            <IonHeader>
+              <IonToolbar>
+                <IonTitle>{!isMenuCollapsed && 'Menu'}</IonTitle>
+                <IonButton
+                  slot="end"
+                  fill="clear"
+                  onClick={() => setIsMenuCollapsed(!isMenuCollapsed)}
+                  aria-label={isMenuCollapsed ? 'Expand menu' : 'Collapse menu'}
+                >
+                  <IonIcon icon={isMenuCollapsed ? chevronForward : chevronBack} />
+                </IonButton>
+              </IonToolbar>
+            </IonHeader>
+            <IonContent>
+              <IonList>
+                <IonItem routerLink="/tab1">
+                  <IonIcon aria-hidden="true" slot="start" icon={triangle} />
+                  {!isMenuCollapsed && <IonLabel>Tab 1</IonLabel>}
+                </IonItem>
+                <IonItem routerLink="/tab2">
+                  <IonIcon slot="start" icon={images} />
+                  {!isMenuCollapsed && <IonLabel>Photos</IonLabel>}
+                </IonItem>
+                <IonItem routerLink="/tab3">
+                  <IonIcon aria-hidden="true" slot="start" icon={square} />
+                  {!isMenuCollapsed && <IonLabel>Tab 3</IonLabel>}
+                </IonItem>
+              </IonList>
+            </IonContent>
+          </IonMenu>
+          {isMenuCollapsed && <IonButton
+            fill="clear"
+            onClick={() => setIsMenuCollapsed(!isMenuCollapsed)}
+            style={{ '--max-width': '10px' }}
+            aria-label={isMenuCollapsed ? 'Expand menu' : 'Collapse menu'}
+          >
+            <IonIcon icon={isMenuCollapsed ? chevronForward : chevronBack} />
+          </IonButton>}
+          <IonRouterOutlet id="main">
+            <Route exact path="/tab1">
+              <Tab1 />
+            </Route>
+            <Route exact path="/tab2">
+              <Tab2 />
+            </Route>
+            <Route path="/tab3">
+              <Tab3 />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tab1" />
+            </Route>
+          </IonRouterOutlet>
+        </IonSplitPane>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
